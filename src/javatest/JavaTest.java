5ef884d99bc3a94400cd2cf12be91fbf61033d3b
Chathu <chathu.git93@gmail.com>
2018-03-21 14:29:38 +0530

    More Tests

--- src/javatest/JavaTest.java
+++ src/javatest/JavaTest.java
@@ -9,17 +9,29 @@ public class JavaTest {
 
     
     public static void main(String[] args) {
-        System.out.println("Person\tHeight\tShoeSize");
-        System.out.println("==========================");
-        System.out.println("Hannah\t5'1\"\t7");
-        System.out.println("Jenna\t5'10\"\t9");
-        System.out.println("JJ\t6'1\"\t14");
-        double d = 4.34;
-        double c = 5.76;
-        int i = (int)d;
-        int j = (int)c;
-        System.out.println(i);
-        System.out.println(j);
+//        System.out.println("Person\tHeight\tShoeSize");
+//        System.out.println("==========================");
+//        System.out.println("Hannah\t5'1\"\t7");
+//        System.out.println("Jenna\t5'10\"\t9");
+//        System.out.println("JJ\t6'1\"\t14");
+//        double d = 4.34;
+//        double c = 5.76;
+//        int i = (int)d;
+//        int j = (int)c;
+//        System.out.println(i);
+//        System.out.println(j);
+
+          String num = "123";
+          System.out.println("Max Double Value: "+Double.MAX_VALUE);
+          System.out.println("Min Byte Value: "+Byte.MIN_VALUE);
+          System.out.println("Converted String: "+Integer.parseInt(num));
+          
+          int arr[] = new int[20];
+          for(int i=0;i<arr.length;i++)
+          {
+              arr[i]= i*2 ;
+              //System.out.println(arr[i]);
+          }
     }
     
 }
