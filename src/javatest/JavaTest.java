35a9972ca49d28218ae4c167637b76c101a00da0
Chathu <chathu.git93@gmail.com>
2018-03-21 13:47:05 +0530

    Initial commit

---
+++ src/javatest/JavaTest.java
@@ -0,0 +1,25 @@
+
+package javatest;
+
+/**
+ *
+ * @author Chathu
+ */
+public class JavaTest {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Person\tHeight\tShoeSize");
+        System.out.println("==========================");
+        System.out.println("Hannah\t5'1\"\t7");
+        System.out.println("Jenna\t5'10\"\t9");
+        System.out.println("JJ\t6'1\"\t14");
+        double d = 4.34;
+        double c = 5.76;
+        int i = (int)d;
+        int j = (int)c;
+        System.out.println(i);
+        System.out.println(j);
+    }
+    
+}
